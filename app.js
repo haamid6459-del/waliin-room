120ada03030dfdbab1711a76ab5341c167f1dfcb
haamid6459-del <haamid6459@gmail.com>
2026-09-19 22:12:37 +0300

    Initialize Waliin Room server with Socket.IO

--- app.js
+++ app.js
@@ -1,1 +1,68 @@
 
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Waliin Room Server is running!");
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+
+    console.log(`${socket.id} joined room ${roomId}`);
+
+    socket.to(roomId).emit("user-connected", socket.id);
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).emit("user-disconnected", socket.id);
+      console.log(`${socket.id} left room ${roomId}`);
+    });
+  });
+
+  socket.on("offer", ({ to, offer }) => {
+    io.to(to).emit("offer", {
+      from: socket.id,
+      offer: offer
+    });
+  });
+
+  socket.on("answer", ({ to, answer }) => {
+    io.to(to).emit("answer", {
+      from: socket.id,
+      answer: answer
+    });
+  });
+
+  socket.on("ice-candidate", ({ to, candidate }) => {
+    io.to(to).emit("ice-candidate", {
+      from: socket.id,
+      candidate: candidate
+    });
+  });
+
+  socket.on("chat-message", ({ room, message }) => {
+    io.to(room).emit("chat-message", {
+      message: message
+    });
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Waliin Room server running on port ${PORT}`);
+});
